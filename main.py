ba2f04fa1a367006ffb03a1f4dfd3d2aadfa84af
Ansh Chaturvedi <a4chatur@uwaterloo.ca>
2024-12-12 19:06:16 -0500

    replicate WS API requests using `cloudscraper`

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import requests
-import wealthsimple
 import pyotp
 import dotenv
 import os
 import json
+import cloudscraper
 
 # load environment variables
 dotenv.load_dotenv()
@@ -11,17 +10,27 @@ TOTP_SECRET = os.getenv("TOTP_SECRET")
 WS_LOGIN_EMAIL = os.getenv("WEALTHSIMPLE_LOGIN_EMAIL")
 WS_LOGIN_PASSWORD = os.getenv("WEALTHSIMPLE_LOGIN_PASSWORD")
 
-totp = pyotp.TOTP(TOTP_SECRET)
+# local constants
+WS_BASE_URL = "https://trade-service.wealthsimple.com/"
 
+# we use `cloudscraper` to get past Cloudflare's anti-bot page
+scraper = cloudscraper.create_scraper()
 
 def get_two_factor_otp():
+    totp = pyotp.TOTP(TOTP_SECRET)
     return totp.now()
 
 
-ws = wealthsimple.WSTrade(
-    WS_LOGIN_EMAIL, WS_LOGIN_PASSWORD, two_factor_callback=get_two_factor_otp
-)
+# authenticate
+params = [
+    ("email", WS_LOGIN_EMAIL),
+    ("password", WS_LOGIN_PASSWORD),
+    ("otp", get_two_factor_otp()),
+]
+response = scraper.post(f"{WS_BASE_URL}/auth/login", params)
+scraper.headers.update({"Authorization": response.headers["X-Access-Token"]})
 
-a = ws.get_accounts()
-with open("output.json", "w") as f:
-    json.dump(a, f)
+# get accounts list
+accounts = scraper.get(f"{WS_BASE_URL}/account/list").json()["results"]
+with open("accounts.json", 'w') as f:
+	json.dump(accounts, f)
